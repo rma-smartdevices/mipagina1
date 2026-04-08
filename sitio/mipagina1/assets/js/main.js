a143f2aafe6708edd49d2c3bcd06c6df8fb62eca
ThriveNFC <54373905+rma-smartdevices@users.noreply.github.com>
2026-04-08 13:50:24 +0000

    Publicar mi sitio

---
+++ sitio/mipagina1/assets/js/main.js
@@ -0,0 +1,17 @@
+console.log("JavaScript conectado ✅");
+
+const reveals = document.querySelectorAll(".reveal");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("is-visible");
+        observer.unobserve(entry.target);
+      }
+    });
+  },
+  { threshold: 0.15 },
+);
+
+reveals.forEach((el) => observer.observe(el));
